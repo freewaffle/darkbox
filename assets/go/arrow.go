0b2e47bb7e691041347efb8264f0dae35de880d7
kandelasky <curtis.cmux@ya.ru>
2025-09-16 17:39:33 +0400

    add

---
+++ assets/go/arrow.go
@@ -0,0 +1,22 @@
+embedded_components {
+  id: "arrow"
+  type: "sprite"
+  data: "default_animation: \"arrow\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 100.0\n"
+  "  y: 100.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/atlas/go.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    y: -35.0
+  }
+  scale {
+    x: 0.5
+    y: 0.5
+  }
+}
