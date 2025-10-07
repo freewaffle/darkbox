3beff0470d142df0f9ecf6ca89a5d17bb21c3399
freewaffle <curtis.cmux@ya.ru>
2025-10-07 16:56:46 +0400

    move 'ttf' (renamed to 'fonts'), 'images', 'sounds' to assets/raw

--- assets/go/sound.go
+++ assets/go/sound.go
@@ -1,49 +1,49 @@
 embedded_components {
   id: "sp1"
   type: "sound"
-  data: "sound: \"/assets/sounds/misc/sp/sp1.ogg\"\n"
+  data: "sound: \"/assets/raw/sounds/misc/sp/sp1.ogg\"\n"
   "group: \"effects\"\n"
   ""
 }
 embedded_components {
   id: "sp2"
   type: "sound"
-  data: "sound: \"/assets/sounds/misc/sp/sp2.ogg\"\n"
+  data: "sound: \"/assets/raw/sounds/misc/sp/sp2.ogg\"\n"
   "group: \"effects\"\n"
   ""
 }
 embedded_components {
   id: "sp3"
   type: "sound"
-  data: "sound: \"/assets/sounds/misc/sp/sp3.ogg\"\n"
+  data: "sound: \"/assets/raw/sounds/misc/sp/sp3.ogg\"\n"
   "group: \"effects\"\n"
   ""
 }
 embedded_components {
   id: "sp4"
   type: "sound"
-  data: "sound: \"/assets/sounds/misc/sp/sp4.ogg\"\n"
+  data: "sound: \"/assets/raw/sounds/misc/sp/sp4.ogg\"\n"
   "group: \"effects\"\n"
   ""
 }
 embedded_components {
   id: "sp5"
   type: "sound"
-  data: "sound: \"/assets/sounds/misc/sp/sp5.ogg\"\n"
+  data: "sound: \"/assets/raw/sounds/misc/sp/sp5.ogg\"\n"
   "group: \"effects\"\n"
   ""
 }
 embedded_components {
   id: "sp6"
   type: "sound"
-  data: "sound: \"/assets/sounds/misc/sp/sp6.ogg\"\n"
+  data: "sound: \"/assets/raw/sounds/misc/sp/sp6.ogg\"\n"
   "group: \"effects\"\n"
   ""
 }
 embedded_components {
   id: "sp7"
   type: "sound"
-  data: "sound: \"/assets/sounds/misc/sp/sp7.ogg\"\n"
+  data: "sound: \"/assets/raw/sounds/misc/sp/sp7.ogg\"\n"
   "group: \"effects\"\n"
   ""
 }
